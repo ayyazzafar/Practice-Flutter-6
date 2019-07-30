777e7093cfe36be2463fc35e43311a700852cd8b
Ayyaz Zafar (Ayyaz) <ayyaz.zaffar@gmail.com>
2019-07-30 20:41:07 +0500

    code

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,119 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
+main() => runApp(MyApp());
 
-void main()=> runApp(HomePage());
-
-
-class HomePage extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        accentColor: Colors.deepOrangeAccent
+      title: 'Flutter Experiments',
+      theme: ThemeData(),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text('Flutter Experiments',
+            style: TextStyle(color: Colors.white, letterSpacing: 1.0)),
+        backgroundColor: new Color(0xFF2979FF),
+        centerTitle: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('Custom shapes'),),
-        body: Padding(padding: EdgeInsets.all(8.0),
-        child: 
-          CustomPaint(
-            painter: ShapesPainter(),
-            child: Container( )
-          )
-        ,),
+      body: HomeContent()
+    );
+  }
+}
+
+class HomeComponent extends StatefulWidget{
+
+  @override
+  _HomeComponentState createState() {
+    // TODO: implement createState
+    return _HomeComponentState();
+  }
+}
+
+class _HomeComponentState extends State<HomeComponent>{
+  double  percentage;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    percentage  = 0.0;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        child: CustomPaint(
+          foregroundPainter: MyPainter(),
+        ),
       ),
     );
   }
 }
 
+class MyPainter extends CustomPainter{
 
-class ShapesPainter extends CustomPainter {
+  Color lineColor;
+  Color completeColor;
+  double completePercentage;
+  double width;
+
+  MyPainter( 
+    {
+      this.lineColor,
+      this.completeColor,
+      this.completePercentage,
+      this.width
+    }
+  );
 
   @override
   void paint(Canvas canvas, Size size) {
+    Paint line  = Paint()
+    ..color = lineColor
+    ..strokeCap = StrokeCap.round
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = width;
 
-    
-      final paint = Paint();
+    Paint complete  = Paint()
+    ..color = completeColor
+    ..strokeCap = StrokeCap.round
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = width;
 
-      paint.color = Colors.white;
-      var rect =  Rect.fromLTWH(0, 0, size.width, size.height);
-      canvas.drawRect(rect, paint);
+    Offset center = Offset(size.width/2, size.height/2);
+    double radius = min(size.width/2, size.height/2);
+    canvas.drawCircle(center, radius, line);
 
+    double arcAngle = 2*pi*(completePercentage/100);
+    canvas.drawArc(Rect.fromCircle( center: center,radius: radius), - pi/2, arcAngle, false, complete);
 
-      
-
-      paint.color = Colors.yellow;
-      var path = Path();
-      path.lineTo(0, size.height);
-      path.lineTo(size.width, 0);
-
-      path.close();
-
-      canvas.drawPath(path, paint);
-
-
-      paint.color = Colors.deepOrange;
-      var center = Offset(size.width/2, size.height/2);
-      canvas.drawCircle(center, 75.0, paint);
-
-      
+   
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
 
 }
